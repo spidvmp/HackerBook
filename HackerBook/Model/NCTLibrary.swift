71b451d8d80df064c05470d8addbd200795ecdb8
Vicente de Miguel <spidvmp@gmail.com>
2015-11-23 14:29:15 +0100

    funciones de libreria de bookCountForTag y booksForTag

--- HackerBook/Model/NCTLibrary.swift
+++ HackerBook/Model/NCTLibrary.swift
@@ -32,23 +32,34 @@ class NCTLibrary {
     }
     
     //MARK: - Methods
-    func bookCountForTag(tag: String?) -> Int{
+    func bookCountForTag(tag: String) -> Int{
         /*
         recibe un string como tag, cuenta cuantos libros tienen ese tag y devuelve lo que ha contado
         */
         
-        return 1
+        return (self.booksForTag(tag)?.count)!
+
     }
     
-    func booksForTag(tag:String?) -> [NCTBook]? {
+    func booksForTag(tag:String) -> [NCTBook]? {
         /*
         recibe un String como tag y devuelve un array con todos los libros que tienene ese tag
         */
-        return nil
+        var librosConTag:[NCTBook]?
+        
+        //me recorro toda la libreria y bisco si en el tag en el array de tags
+        for l in libros {
+            let li = l as? NCTBook
+            //compruebo si tiene el tag
+            if ( l.tags.contains(tag)) {
+                librosConTag?.append(li!)
+            }
+        }
+        return librosConTag
         
     }
     
-    func bookAtIndex(tag:String?, index: Int) -> NCTBook? {
+    func bookAtIndex(tag:String, index: Int) -> NCTBook? {
         /*
         Recibe un String como tag y un indice, busca los libros con ese tag y devuelve el que este en el orden index
         */
